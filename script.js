35443d3bfd9f7f3895f88bbc0c16f80c3f4ad8f1
Depoule <jpoulet02100@gmail.com>
2019-01-28 10:56:45 +0100

    exo 2.2 fini et corrigé

---
+++ script.js
@@ -0,0 +1,5 @@
+// je déclare ma fonction unFocus => retirer le focus en francais
+// pour que le message d'alerte s'affiche lorsque je clique hors de ma zone de texte
+function unFocus() {
+    alert('Merci de votre participation');
+}
